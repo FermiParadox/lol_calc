2e82a1cc65b996b4796a5b51a7c2b509ee05e6b1
black <j0hnWh1t3@outlook.com>
2015-04-23 22:14:02 +0300

    WARNING: Enclosed scope variables. (Probable bugs.) Dict references instead of copies. (Probable bugs.) a=1 -> a==True (Probable bugs.)
    CHANGES:
    -Working on items module.
    -Methods in items module complete (NOT TESTED).
    
    NOW:
    Fix items effects.
    Make dmgs able to apply buffs or other dmgs.
    New methods in buffs/dmg categories (on_hit).
    
     LATER:
     Create bonus_mr, +_hp, +_armor stat methods.
     Create check for same dmg names for different dmgs (eg. dmg_0 for an item and for a champ dmg)
         since it would cause bugs with total dmg tracking.
     Allow independent actions.
     Create action priority functionality (alternative option to a set rotation).
     Improve Kalista movement after AA (include a small delay before walking is allowed if needed).
     Make modules containing api data related to a champion's spells contain "passive" as well (since its separate on API).
     Make movement counter accurate (must refresh buffs that have speed change, must account for time decaying speed buffs).
     Gather live data and compare them to result (OCR).
     Check if real AA dmg deviation is due to not rounding reductions.

--- items.py
+++ items.py
@@ -12,7 +12,7 @@ class ItemsProperties(object):
         self.items_effects_dct = items_data_module.ITEMS_EFFECTS
         self.items_conditions_dct = items_data_module.ITEMS_CONDITIONS
 
-    def leafs_used(self, item_name):
+    def leafs_of_item(self, item_name):
         """
         Returns items' names that are build from given item and are part of the chosen build.
 
@@ -22,9 +22,42 @@ class ItemsProperties(object):
 
         return self.items_attrs_dct[item_name]['secondary_data']['leafs'] & set(self.chosen_items_lst)
 
+    def unique_stats_in_leafs_of_item(self, item_name):
+        """
+        Returns a dict of all additive and all percent unique stats' names of an item's leafs.
+
+        :param item_name: (str)
+        :return: (dict)
+        """
+        used_leafs = self.leafs_of_item(item_name)
+        returned_dct = {'additive': [], 'percent': []}
+
+        # For each leaf-item..
+        for leaf in used_leafs:
+            leaf_dct = self.items_attrs_dct[leaf]
+            # ..for each stat type..
+            for stat_type in leaf_dct:
+                # ..creates a list of the stat names and adds it in existing results.
+                returned_dct[stat_type] += list(leaf_dct['unique_stats'][stat_type].keys())
+
+        return returned_dct
+
+    def non_roots_in_build(self):
+        """
+        Returns items that are not the base for any of items in current build.
+
+        :return: (list)
+        """
+
+        all_roots = {}
+        for item in self.chosen_items_lst:
+            all_roots |= items_data_module.ITEMS_ATTRIBUTES[item]['secondary_stats']
+
+        return set(self.chosen_items_lst) - all_roots
+
     def _create_items_properties_dcts(self):
         """
-        Creates final dicts (effects, attributes, dmgs, buffs, conditions) for given item build.
+        Creates final dicts (effects, attributes, dmgs, buffs, conditions) for current item build.
 
         :return: (None)
         """
@@ -34,16 +67,24 @@ class ItemsProperties(object):
             item_effects = self.items_effects_dct[item_name]
             item_conditions = self.items_conditions_dct[item_name]
 
-            # Stacking stats.
+            # NON UNIQUE STATS
             item_non_unique_stats = item_attrs['non_unique_stats']
             for stat_type in item_non_unique_stats:
                 self.non_unique_stats_dct[stat_type] += collections.Counter(item_non_unique_stats[stat_type])
 
-            # Unique stats.
+            # UNIQUE STATS
+            # If item A (leaf) builds from item B, then unique stats from B that are included in A are ignored.
             unique_stats_dct = item_attrs['unique_stats']
-            for stat_type in item_non_unique_stats:
-                self.unique_stats_dct[stat_type] += collections.Counter(unique_stats_dct[stat_type])
+            leafs_stats = self.unique_stats_in_leafs_of_item(item_name=item_name)
 
+            for stat_type in unique_stats_dct:
+                for stat_name in unique_stats_dct[stat_type]:
+                    # If stat is no inside leafs' stats.
+                    if stat_name not in leafs_stats[stat_type]:
+                        self.unique_stats_dct[stat_type].update({stat_name: unique_stats_dct[stat_type][stat_name]})
+
+            # EFFECTS
+            # Root effects are ignored, since leaf is (assumed to) override them.
 
 
 
@@ -81,4 +122,9 @@ if __name__ == '__main__':
     testBuildCost = True
     if testBuildCost:
         g = ItemsProperties(['hextech_gunblade', 'dorans_blade']).build_price()
+        print(g)
+
+    testUniqueLeafsUniqueStats = True
+    if testUniqueLeafsUniqueStats:
+        g = ItemsProperties(['hextech_gunblade', 'dorans_blade']).unique_stats_in_leafs_of_item(item_name='dorans_blade')
         print(g)
